4fc2d8ad7b56e9e37eaefb4cfcd34c320054a992
HeYiCheng1X <1424864061@qq.com>
2021-06-17 08:48:48 +0800

    将Tab接口删除，改为自己实现的tab

--- myapp/app/src/main/java/com/example/myapp/activity/HomeActivity.java
+++ myapp/app/src/main/java/com/example/myapp/activity/HomeActivity.java
@@ -2,59 +2,97 @@ package com.example.myapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 
 import com.example.myapp.R;
-import com.example.myapp.adapter.MyPagerAdapter;
-import com.example.myapp.entity.TabEntity;
+import com.example.myapp.adapter.MyFragmentPagerAdapter;
 import com.example.myapp.fragment.CollectFragment;
 import com.example.myapp.fragment.HomeFragment;
 import com.example.myapp.fragment.MyFragment;
-import com.flyco.tablayout.CommonTabLayout;
-import com.flyco.tablayout.listener.CustomTabEntity;
-import com.flyco.tablayout.listener.OnTabSelectListener;
 
 import java.util.ArrayList;
 
-public class HomeActivity extends AppCompatActivity {
-    private String[] mTitles = {"首页", "资讯", "我的"};
-    private int[] mIconUnselectIds = {
-            R.mipmap.home_unselect, R.mipmap.collect_unselect,
-            R.mipmap.my_unselect};
-    private int[] mIconSelectIds = {
-            R.mipmap.home_selected, R.mipmap.collect_selected,
-            R.mipmap.my_selected};
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+    private ViewPager2 viewPager2;
+    private MyFragmentPagerAdapter pagerAdapter;
+    private RadioButton rb_home, rb_search, rb_my, rb_current;
+
+    private String[] mTitles = {"首页", "搜索", "我的"};
     private ArrayList<Fragment> mFragments = new ArrayList<>();
-    private ArrayList<CustomTabEntity> mTabEntities = new ArrayList<>();
-    private ViewPager viewPager;
-    private CommonTabLayout commonTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        viewPager = findViewById(R.id.viewpager);
-        commonTabLayout = findViewById(R.id.commonTabLayout);
+        initPager();
+        initTabView();
+    }
 
+    //管理Fragment
+    private void initPager() {
+        viewPager2 = findViewById(R.id.viewpager2);
         mFragments.add(HomeFragment.newInstance());
         mFragments.add(CollectFragment.newInstance());
         mFragments.add(MyFragment.newInstance());
-        for (int i = 0; i < mTitles.length; i++) {
-            mTabEntities.add(new TabEntity(mTitles[i], mIconSelectIds[i], mIconUnselectIds[i]));
-        }
-        commonTabLayout.setTabData(mTabEntities);
-        commonTabLayout.setOnTabSelectListener(new OnTabSelectListener() {
-            @Override
-            public void onTabSelect(int position) {
-                viewPager.setCurrentItem(position);
-            }
 
+        pagerAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager(), getLifecycle(), mFragments, mTitles);
+        viewPager2.setAdapter(pagerAdapter);
+        viewPager2.setUserInputEnabled(false);//设置禁止滑动
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             @Override
-            public void onTabReselect(int position) {
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                changeTab(position);
             }
         });
-        viewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager(), mTitles, mFragments));
+    }
+
+    //底部UI管理
+    private void initTabView() {
+        rb_home = findViewById(R.id.tab_home);
+        rb_search = findViewById(R.id.tab_search);
+        rb_my = findViewById(R.id.tab_mine);
+
+        rb_home.setOnClickListener(this);
+        rb_search.setOnClickListener(this);
+        rb_my.setOnClickListener(this);
+
+        rb_home.setSelected(false);
+        rb_current = rb_home;
+    }
+
+    private void changeTab(int position) {
+        //rb_current为当前的按钮
+        rb_current.setSelected(false);
+        switch (position) {
+            case R.id.tab_home:
+                viewPager2.setCurrentItem(0, false);
+            case 0:
+                rb_home.setSelected(true);
+                rb_current = rb_home;
+                break;
+            case R.id.tab_search:
+                viewPager2.setCurrentItem(1, false);
+            case 1:
+                rb_search.setSelected(true);
+                rb_current = rb_search;
+                break;
+            case R.id.tab_mine:
+                viewPager2.setCurrentItem(2, false);
+            case 2:
+                rb_my.setSelected(true);
+                rb_current = rb_my;
+                break;
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        changeTab(view.getId());
     }
 }
---
+++ myapp/app/src/main/java/com/example/myapp/adapter/MyFragmentPagerAdapter.java
@@ -0,0 +1,43 @@
+package com.example.myapp.adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.example.myapp.fragment.CollectFragment;
+import com.example.myapp.fragment.HomeFragment;
+import com.example.myapp.fragment.MyFragment;
+
+import java.util.ArrayList;
+
+public class MyFragmentPagerAdapter extends FragmentStateAdapter {
+    ArrayList<Fragment> mFragments;
+    String[] mTitles;
+
+    public MyFragmentPagerAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle, ArrayList<Fragment> mFragments, String[] mTitles) {
+        super(fragmentManager, lifecycle);
+        this.mFragments = mFragments;
+        this.mTitles = mTitles;
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return new HomeFragment();
+            case 1:
+                return new CollectFragment();
+            case 2:
+                return new MyFragment();
+        }
+        return null;
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
